6537cc6c83f6ca2b5825a908eab93e63858798ae
huangqingyou123 <438876653@qq.com>
2016-04-12 17:15:43 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "438876653@qq.com"
+git config --global user.name "huangqingyou123"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start https://huangqingyou123.github.io
